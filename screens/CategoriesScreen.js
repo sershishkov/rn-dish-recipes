ae4aa26dc81a12e392b339286ae3a75ce6b2e044
sershishkov <ser.shishkov@gmail.com>
2019-09-16 18:52:39 +0300

    6-12

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderFridItem = itemData => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
+
 const CategoriesScreen = props => {
-  return <FlatList numColumns={2} />;
+  return (
+    <FlatList
+      keyExtractor={item => item.id}
+      data={CATEGORIES}
+      numColumns={2}
+      renderItem={renderFridItem}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
@@ -10,6 +27,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
 
