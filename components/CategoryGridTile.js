ed9e2038404f7ddd3bfcb38af4315d2a198982b6
sershishkov <ser.shishkov@gmail.com>
2019-09-24 19:13:01 +0300

    6-24

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -34,7 +34,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden'
+    overflow:
+      Platform.OS === 'android' && Platform.Version >= 21
+        ? 'hidden'
+        : 'visible',
+    elevation: 5
     // borderColor: 'black',
     // borderWidth: 1
   },
@@ -45,7 +49,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
+    // elevation: 3,
     padding: 15,
     alignItems: 'flex-end',
     justifyContent: 'flex-end'
