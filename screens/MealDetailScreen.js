7d9bf8bf17b349fa864063c94d04f34387059822
sershishkov <ser.shishkov@gmail.com>
2019-09-24 08:00:45 +0300

    6-23

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
@@ -23,7 +25,18 @@ MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam('mealId');
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title='Favorite'
+          iconName='ios-star'
+          onPress={() => {
+            console.log('Mark as favorite!');
+          }}
+        />
+      </HeaderButtons>
+    )
   };
 };
 
