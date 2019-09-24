fe78be9afb88965f260878431e4749db4657219e
sershishkov <ser.shishkov@gmail.com>
2019-09-24 19:42:01 +0300

    6-25

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -2,11 +2,12 @@ import { Platform } from 'react-native';
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
-// import FavoritesScreen from '../screens/FavoritesScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 // import FiltersScreen from '../screens/FiltersScreen';
 import Colors from '../constants/Colors';
 
@@ -32,4 +33,9 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen
+});
+
+export default createAppContainer(MealsFavTabNavigator);
