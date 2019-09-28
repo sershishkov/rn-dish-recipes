91c71bdbe4a5d2bdab874fa7bd256d9bf4abdfce
sershishkov <ser.shishkov@gmail.com>
2019-09-28 17:51:14 +0300

    7-8

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { View, Text, StyleSheet } from 'react-native';
 
 import MealList from '../components/MealList';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some </DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
@@ -29,5 +38,12 @@ FavoritesScreen.navigationOptions = navData => {
     )
   };
 };
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default FavoritesScreen;
