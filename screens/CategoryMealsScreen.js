a38f9bcdf788b9238ed2d3724ea3898fed997494
sershishkov <ser.shishkov@gmail.com>
2019-09-28 18:38:09 +0300

    7-12

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { View, StyleSheet } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealsScreen = props => {
   const catId = props.navigation.getParam('categoryId');
@@ -12,6 +14,13 @@ const CategoryMealsScreen = props => {
   const displayMeals = avaliableMeals.filter(
     meal => meal.categoryId.indexOf(catId) >= 0
   );
+  if (displayMeals.length === 0 || !displayMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText> No meals found, maybe check your filters</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={displayMeals} navigation={props.navigation} />;
 };
@@ -24,5 +33,13 @@ CategoryMealsScreen.navigationOptions = navigationData => {
     headerTitle: selectedCategory.title
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default CategoryMealsScreen;
